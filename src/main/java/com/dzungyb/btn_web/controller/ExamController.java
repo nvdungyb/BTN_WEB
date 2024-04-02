213e4c99cfed44b9ba8c3e5694fc4746b10b0cfc
NvDzung <dung3032003md@gmail.com>
2024-04-02 08:24:29 +0700

    Update

--- src/main/java/com/dzungyb/btn_web/controller/ExamController.java
+++ src/main/java/com/dzungyb/btn_web/controller/ExamController.java
@@ -1,16 +1,11 @@
-package com.examBE.BackendExamSys.controllers;
+package com.dzungyb.btn_web.controller;
 
-import com.examBE.BackendExamSys.entities.ExamEntity;
-import com.examBE.BackendExamSys.models.ApiResult;
-import com.examBE.BackendExamSys.models.Dto.ExamDto;
-import com.examBE.BackendExamSys.models.Dto.ExamQuestionDto;
-import com.examBE.BackendExamSys.models.Dto.UserDto;
-import com.examBE.BackendExamSys.models.ExamDetailModel;
-import com.examBE.BackendExamSys.services.ExamService;
-import com.examBE.BackendExamSys.utils.Constants;
+import com.dzungyb.btn_web.entity.Exam;
+import com.dzungyb.btn_web.model.ExamDetailModel;
+import com.dzungyb.btn_web.model.dto.ExamDto;
+import com.dzungyb.btn_web.model.dto.ExamQuestionDto;
+import com.dzungyb.btn_web.service.ExamService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,115 +13,107 @@ import java.util.List;
 
 @Controller
 @RequestMapping(path = "/exams")
-@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ExamController {
     private final ExamService examService;
 
     @GetMapping("/list")
-    public ResponseEntity<ApiResult<List<ExamEntity>>> showList() {
-        List<ExamEntity> userList = examService.getAll();
+    public @ResponseBody List<Exam> showList() {
+        List<Exam> examList = examService.getAll();
         String message = "Success";
-        if (userList.isEmpty()) {
+        if (examList.isEmpty()) {
             message = "Empty";
         }
-        ApiResult<List<ExamEntity>> result = ApiResult.create(HttpStatus.OK, message, userList);
-        return ResponseEntity.ok(result);
+
+        return examList;
     }
+
     @GetMapping("/listDetail")
-    public ResponseEntity<ApiResult<List<ExamDetailModel>>> showListDetail() {
-        List<ExamDetailModel> userList = examService.getAllDetail();
+    public @ResponseBody List<ExamDetailModel> showListDetail() {
+        List<ExamDetailModel> examDetailList = examService.getAllDetail();
         String message = "Success";
-        if (userList.isEmpty()) {
+        if (examDetailList.isEmpty()) {
             message = "Empty";
         }
-        ApiResult<List<ExamDetailModel>> result = ApiResult.create(HttpStatus.OK, message, userList);
-        return ResponseEntity.ok(result);
+        return examDetailList;
     }
+
     @GetMapping(path = "/searchById")
-    public ResponseEntity<ApiResult<ExamEntity>> searchById(@RequestParam(name = "id", required = false, defaultValue = "") int id) {
-        ApiResult<ExamEntity> result = null;
-        try{
-            ExamEntity exam = examService.searchById(id);
-            result = ApiResult.create(HttpStatus.OK, Constants.SEARCH_DATA_SUCCESS, exam);
+    public @ResponseBody Exam searchById(@RequestParam(name = "id", required = false, defaultValue = "") int id) {
+        Exam result = null;
+        try {
+            result = examService.searchById(id);
+        } catch (Exception ex) {
+            result = null;
         }
-        catch (Exception ex) {
-            result = ApiResult.create(HttpStatus.CONFLICT, ex.getMessage(), null);
-        }
-        return ResponseEntity.ok(result);
+        return result;
     }
+
     @GetMapping(path = "/searchDetailById")
-    public ResponseEntity<ApiResult<ExamDetailModel>> searchDetailById(@RequestParam(name = "id", required = false, defaultValue = "") int id) {
-        ApiResult<ExamDetailModel> result = null;
-        try{
+    public @ResponseBody ExamDetailModel earchDetailById(@RequestParam(name = "id", required = false, defaultValue = "") int id) {
+        ExamDetailModel result = null;
+        try {
             ExamDetailModel exam = examService.searchDetailById(id);
-            result = ApiResult.create(HttpStatus.OK, Constants.SEARCH_DATA_SUCCESS, exam);
+            result = exam;
+        } catch (Exception ex) {
+            result = null;
         }
-        catch (Exception ex) {
-            result = ApiResult.create(HttpStatus.CONFLICT, ex.getMessage(), null);
-        }
-        return ResponseEntity.ok(result);
+        return result;
     }
+
     @PostMapping(path = "/create")
-    public ResponseEntity<ApiResult<ExamDto>> create(@RequestBody ExamDto examDto) {
-        ApiResult<ExamDto> result = null;
-        try{
+    public @ResponseBody ExamDto create(@RequestBody ExamDto examDto) {
+        ExamDto result = null;
+        try {
             examService.create(examDto);
-            result = ApiResult.create(HttpStatus.OK, Constants.SAVE_DATA_SUCCESS, examDto);
+            result = examDto;
+        } catch (Exception ex) {
         }
-        catch (Exception ex) {
-            result = ApiResult.create(HttpStatus.CONFLICT, ex.getMessage(), examDto);
-        }
-        return ResponseEntity.ok(result);
+        return result;
     }
+
     @PostMapping(path = "/createAll")
-    public ResponseEntity<ApiResult<ExamQuestionDto>> createAll(@RequestBody ExamQuestionDto examQuestionDto) {
-        ApiResult<ExamQuestionDto> result = null;
-        try{
+    public @ResponseBody ExamQuestionDto createAll(@RequestBody ExamQuestionDto examQuestionDto) {
+        ExamQuestionDto result = null;
+        try {
             examService.createAll(examQuestionDto);
-            result = ApiResult.create(HttpStatus.OK, Constants.SAVE_DATA_SUCCESS, examQuestionDto);
+            result = examQuestionDto;
+        } catch (Exception ex) {
         }
-        catch (Exception ex) {
-            result = ApiResult.create(HttpStatus.CONFLICT, ex.getMessage(), examQuestionDto);
-        }
-        return ResponseEntity.ok(result);
+        return result;
     }
+
     @PutMapping(path = "/update")
-    public ResponseEntity<ApiResult<ExamEntity>> update(@RequestBody ExamEntity examEntity) {
-        ApiResult<ExamEntity> result = null;
-        try{
+    public @ResponseBody Exam update(@RequestBody Exam examEntity) {
+        Exam result = null;
+        try {
             examService.edit(examEntity);
-            result = ApiResult.create(HttpStatus.OK, Constants.EDIT_DATA_SUCCESS, examEntity);
+            result = examEntity;
+        } catch (Exception ex) {
+            result = null;
         }
-        catch (Exception ex) {
-            result = ApiResult.create(HttpStatus.CONFLICT, ex.getMessage(), examEntity);
-        }
-        return ResponseEntity.ok(result);
+        return result;
     }
-    
+
     @PutMapping(path = "/updateAll")
-    public ResponseEntity<ApiResult<ExamDetailModel>> updateAll(@RequestBody ExamDetailModel examDetailModel) {
-        ApiResult<ExamDetailModel> result = null;
-        try{
+    public @ResponseBody ExamDetailModel updateAll(@RequestBody ExamDetailModel examDetailModel) {
+        ExamDetailModel result = null;
+        try {
             examService.editAll(examDetailModel);
-            result = ApiResult.create(HttpStatus.OK, Constants.EDIT_DATA_SUCCESS, examDetailModel);
+            result = examDetailModel;
+        } catch (Exception ex) {
         }
-        catch (Exception ex) {
-            result = ApiResult.create(HttpStatus.CONFLICT, ex.getMessage(), examDetailModel);
-        }
-        return ResponseEntity.ok(result);
+        return result;
     }
 
     @DeleteMapping(path = "/delete")
-    public ResponseEntity<ApiResult<Integer>> delete(@RequestParam(name = "id", required = false, defaultValue = "") int id){
-        ApiResult<Integer> result = null;
-        try{
+    public @ResponseBody Integer delete(@RequestParam(name = "id", required = false, defaultValue = "") int id) {
+        Integer result = null;
+        try {
             examService.delete(id);
-            result = ApiResult.create(HttpStatus.OK, Constants.DELETE_DATA_SUCCESS, id);
+            result = id;
+        } catch (Exception ex) {
         }
-        catch (Exception ex) {
-            result = ApiResult.create(HttpStatus.CONFLICT, ex.getMessage(), id);
-        }
-        return ResponseEntity.ok(result);
+        return result;
     }
 }
